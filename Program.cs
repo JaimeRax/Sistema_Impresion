537e94af9e8d1956dea108845f16b1f3dbc086a5
JaimeRax <jaimecaal60@gmail.com>
2023-08-17 23:02:58 -0600

    Creacion_de_Proyecto

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+// Interfaces segregadas para diferentes formatos de documentos
+
+
+// Clases que implementan las interfaces según el formato de documento
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
